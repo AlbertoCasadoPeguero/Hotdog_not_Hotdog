d76714bd3cb4e019288aae5e445b51faeae7b042
Alberto Casado <albertocasadopeguero@gmail.com>
2020-06-11 23:40:16 -0400

    Create hotdog_not_hotdog.py
    Initial commit of the project creation

---
+++ hotdog_not_hotdog.py
@@ -0,0 +1,25 @@
+#Importing the libraries
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+#Preprocessing training set
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+training_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+#Preprocessing the test set
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
